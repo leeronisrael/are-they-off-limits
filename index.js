39136f29605f2717f21207f35320632596ccac18
Leeron Israel <leeronisrael@LeeronIsrael.local>
2019-04-07 16:18:24 -0400

    add try again button. fix calculation.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var total;
+
 //Calculate Tip
 function calculateResult() {
-  var birthday = document.getElementById("birthday").value;
-  var serious = document.getElementById("serious").value;
-  var stability = document.getElementById("stability").value;
-  var duration = document.getElementById("duration").value;
-  var elapsed = document.getElementById("elapsed").value;
-  var another = document.getElementById("another").value;
+  var birthday = parseInt(document.getElementById("birthday").value);
+  var serious = parseInt(document.getElementById("serious").value);
+  var stability = parseInt(document.getElementById("stability").value);
+  var duration = parseInt(document.getElementById("duration").value);
+  var elapsed = parseInt(document.getElementById("elapsed").value);
+  var another = parseInt(document.getElementById("another").value);
 
   //validate input
   if (birthday == -1 || serious == -1 || stability == -1 || duration == -1 || elapsed == -1 || another == -1 ) {
@@ -14,10 +16,10 @@ function calculateResult() {
   }
 
   //Calculate sum
-  var total = ((birthday + duration + elapsed + (serious * stability)) - another)
+  total = ((birthday + duration + elapsed + (serious * stability)) - another)
 
   //Display the result
-  if (total>-1 && total<=18){
+  if (total<=18){
     document.getElementById("totalResult").style.display = "block"
     document.getElementById("result").innerHTML = "You're good to go!";
   } else if (total>=18){
@@ -29,11 +31,27 @@ function calculateResult() {
   }
 }
 
+function clearResult(){
+  document.getElementById("birthday").value = "-1";
+  document.getElementById("serious").value = "-1";
+  document.getElementById("stability").value = "-1";
+  document.getElementById("duration").value = "-1";
+  document.getElementById("elapsed").value = "-1";
+  document.getElementById("another").value = "-1";
+  document.getElementById("totalResult").style.display = "none";
+  total = -1
+}
 //Hide the result on load
 document.getElementById("totalResult").style.display = "none";
 
 //click to call function
 document.getElementById("calculate").onclick = function() {
   calculateResult();
-
 };
+
+//click to reset
+document.getElementById("back").onclick = function()  {
+  clearResult()
+}
+
+
